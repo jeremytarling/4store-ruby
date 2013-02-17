95bd36e62387f62b391a26b8b87faaf318fc200f
Jeremy Tarling <jeremy.tarling@gmail.com>
2013-02-17 21:26:36 +0000

    added get MP freebase details

---
+++ get_MP_freebase.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require '4store-ruby'
+
+store = FourStore::Store.new 'http://www.topdrawersausage.net:9000/sparql/'
+
+@output = store.select("
+SELECT ?name ?extId WHERE {
+    ?s a <http://www.bbc.co.uk/ontologies/tag/TagConcept> .
+    ?s <http://www.bbc.co.uk/ontologies/domain/canonicalName> ?name .
+    ?s <http://www.bbc.co.uk/ontologies/domain/externalId> ?extId .
+    ?extId <http://www.bbc.co.uk/ontologies/domain/externalIdType> <http://www.bbc.co.uk/things/external-id-types/freebase> .
+} LIMIT 10
+")
+
+puts @output
+
