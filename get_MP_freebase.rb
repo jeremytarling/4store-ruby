85980bf9d01388f5ca3fe041801c635369d9201e
Jeremy Tarling <jeremy.tarling@gmail.com>
2013-02-18 13:23:41 +0000

    tidied up the sparql a bit

--- get_MP_freebase.rb
+++ get_MP_freebase.rb
@@ -5,9 +5,9 @@ store = FourStore::Store.new 'http://www.topdrawersausage.net:9000/sparql/'
 
 @output = store.select("
 SELECT ?name ?extId WHERE {
-    ?s a <http://www.bbc.co.uk/ontologies/tag/TagConcept> .
-    ?s <http://www.bbc.co.uk/ontologies/domain/canonicalName> ?name .
-    ?s <http://www.bbc.co.uk/ontologies/domain/externalId> ?extId .
+    ?s a <http://www.bbc.co.uk/ontologies/tag/TagConcept> ;
+      <http://www.bbc.co.uk/ontologies/domain/canonicalName> ?name ;
+      <http://www.bbc.co.uk/ontologies/domain/externalId> ?extId .
     ?extId <http://www.bbc.co.uk/ontologies/domain/externalIdType> <http://www.bbc.co.uk/things/external-id-types/freebase> .
 } LIMIT 10
 ")
--- get_MP_names.rb
+++ get_MP_names.rb
@@ -4,9 +4,9 @@ require '4store-ruby'
 store = FourStore::Store.new 'http://www.topdrawersausage.net:9000/sparql/'
 
 @output = store.select("
-SELECT DISTINCT ?mpName WHERE {
-    ?guid <http://www.bbc.co.uk/ontologies/domain/canonicalName> ?mpName
-} LIMIT 100
+SELECT ?name ?guid WHERE {
+    ?guid <http://www.bbc.co.uk/ontologies/domain/canonicalName> ?name .
+} LIMIT 10
 ")
 
 puts @output
